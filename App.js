38fff825b6ce4a8c6d584cd8fd23c0b788abf81d
MikeAngelo007 <matovaro@unal.edu.co>
2018-04-11 21:41:05 -0500

    Hora de iniciar con la App

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hola!</Text>
+        <Text>Es hora de iniciar con la app de BiciUn.</Text>
+        <Text>Let's go!.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
