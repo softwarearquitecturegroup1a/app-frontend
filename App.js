deba08c39703b7c2e7ea790383d70a6590975a1c
Alejandro Sierra <gasierram@unal.edu.co>
2018-04-16 23:39:09 -0500

    haciendo login view

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Button, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-
 import DetailsScreen from './vistas/Request'
 import HomeScreen from './vistas/HomeScreen'
 
+import Login from './vistas/Login'
 export default RootStack = StackNavigator({
     Home: {
-      screen: HomeScreen,
+      screen: Login,
     },
     Details: {
-      screen: DetailsScreen,
+      screen: HomeScreen,
     },
   },{
     initialRouteName: 'Home',
